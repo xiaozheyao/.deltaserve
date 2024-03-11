9397d16d615a55d373846dd62ef41719e5633910
Xiaozhe Yao <askxzyao@gmail.com>
2024-03-11 23:26:16 +0100

    add quant linear

--- vllm/deltas/quant_linear.py
+++ vllm/deltas/quant_linear.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch.nn as nn
 import transformers
 from loguru import logger
-from vllm.deltas.utils import make_q_matrix, ext_gemm_half_q_half
+from vllm.deltas.utils import ext_gemm_half_q_half, ext_make_q_matrix
 
 class QuantLinear(nn.Module):
     def __init__(
@@ -46,8 +46,38 @@ class QuantLinear(nn.Module):
             )
         else:
             self.bias = None
+        if self.bits == 4:
+            self.padding = -outfeatures % 32
             
-        if self.bits in [2, 4, 8]:
-            self.wf = torch.tensor(
-                list(range(0, 32, self.bits)), dtype=torch.int32
-            ).unsqueeze(0)
+    def post_init(self, temp_dq):
+        if self.bits == 4:
+            assert self.qweight.device.type == "cuda"
+            assert self.qweight.device.index is not None
+            self.q_tensors = {
+                "qweight": self.qweight,
+                "qzeros": self.qzeros,
+                "scales": self.scales,
+                "g_idx": self.g_idx,
+            }
+            temp_dq = temp_dq.get_scratch_slice(self.temp_dq_size())
+            self.q_handle = ext_make_q_matrix(self.q_tensors, temp_dq)
+        else:
+            raise NotImplementedError("Only 4 bits are supported.")
+    
+    def temp_dq_size(self):
+        return self.infeatures * self.outfeatures * 2 + 128
+
+    def temp_fwd_size(self, max_input_len, max_batch_size):
+        return self.outfeatures * max_input_len * max_batch_size * 4 + 128
+
+    def scratch_space_fixed(self, max_input_len=2048, max_batch_size=8):
+        return self.temp_dq_size() + self.temp_fwd_size(max_input_len, max_batch_size)
+
+    def forward(self, x):
+        if self.bits == 4:
+            output = ext_gemm_half_q_half(x, self.q_handle, self.outfeatures, False)
+            if self.bias:
+                output.add_(self.bias)
+            return output
+        else:
+            raise NotImplementedError("Only 4 bits are supported.")
