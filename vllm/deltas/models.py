306cc3ae06cb030bbfb4c8c9a63cc21e167c8f8b
Xiaozhe Yao <askxzyao@gmail.com>
2024-03-12 17:11:27 +0100

    wip: delta models

---
+++ vllm/deltas/models.py
@@ -0,0 +1,34 @@
+import copy
+import json
+import logging
+import math
+import os
+import re
+from typing import (Any, Callable, Dict, Hashable, List, Optional, Tuple, Type)
+
+import safetensors.torch
+import torch
+from torch import nn
+from vllm.deltas.config import DeltaCompressionConfig
+from vllm.utils import LRUCache, in_wsl
+from vllm.deltas.delta import DeltaLayerWeights
+
+_GLOBAL_DELTA_ID = 0
+
+def get_delta_id():
+    global _GLOBAL_DELTA_ID
+    _GLOBAL_DELTA_ID += 1
+    return _GLOBAL_DELTA_ID
+
+class DeltaModel:
+    """A delta model compressed from the fine-tuned variant"""
+    def __init__(
+        self,
+        delta_model_id: int,
+        deltas: Dict[str, DeltaLayerWeights],
+    ):
+        self.id = delta_model_id
+        self.deltas: Dict[str, DeltaLayerWeights] = deltas
+    
+    def get_lora(self, module_name: str) -> Optional[DeltaLayerWeights]:
+        return self.deltas.get(module_name, None)
