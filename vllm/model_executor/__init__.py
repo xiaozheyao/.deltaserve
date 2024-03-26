dc2d1ca66375b1ac1be72047efd46a1b22da1085
Xiaozhe Yao <askxzyao@gmail.com>
2024-03-26 11:42:19 +0100

    merge

--- vllm/model_executor/__init__.py
+++ vllm/model_executor/__init__.py
@@ -1,7 +1,9 @@
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.model_executor.utils import set_random_seed
+from vllm.model_executor.model_loader import get_model
 
 __all__ = [
     "SamplingMetadata",
     "set_random_seed",
+    "get_model",
 ]
