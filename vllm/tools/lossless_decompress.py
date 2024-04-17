ec843130d221819a49e3f0a9bc1c48534537af2d
Xiaozhe Yao <askxzyao@gmail.com>
2024-04-17 20:13:24 +0200

    lossless decompression in tools

---
+++ vllm/tools/lossless_decompress.py
@@ -0,0 +1,29 @@
+from vllm.delta.compressor import LosslessCompressor
+import safetensors as st
+import json
+import cupy as cp
+from safetensors.torch import save_file
+FILEPATH = "../deltazip/.local/compressed_models/lmsys.vicuna-13b-v1.5.4b50s128g/deltazip-compressed.safetensors"
+
+lc = LosslessCompressor()
+tensors = {}
+with st.safe_open(FILEPATH, "torch") as f:
+    metadata = f.metadata()
+    keys = f.keys()
+    for key in keys:
+        tensors[key] = f.get_tensor(key)
+    tensor_dtypes = json.loads(metadata["dtype"])
+    tensor_shapes = json.loads(metadata["shape"])
+    
+with cp.cuda.Device(0):
+    for key in tensors.keys():
+        tensors[key] = cp.array(tensors[key], copy=False)
+tensors = lc.decompress_state_dict(
+    tensors,
+    tensor_shapes,
+    tensor_dtypes,
+    use_bfloat16=False,
+    target_device="cuda:0",
+)
+# save the decompressed tensors
+save_file(tensors, ".idea/models/vicuna-13b-4b0.75s-decompressed/deltazip-compressed.safetensors")
