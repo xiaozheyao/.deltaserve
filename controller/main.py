e6197b616e7a34616401cce5c5d3b8a00bf93e73
Xiaozhe Yao <askxzyao@gmail.com>
2024-04-28 18:27:45 +0200

    cleanup

--- controller/main.py
+++ controller/main.py
@@ -3,14 +3,20 @@ from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 from starlette.background import BackgroundTask
 from fastapi.responses import StreamingResponse
-
+from .protocols import UpstreamRegistrationRequest
+from .policies.core import uss, UpstreamServer
 app = FastAPI()
+DEFAULT_TIMEOUT = 12000
+upstream_servers = []
 
-@app.post('/{path:path}')
+@app.post('/proxy/{path:path}')
 async def reverse_proxy(request: Request):
-    client = httpx.AsyncClient(base_url="http://127.0.0.1:8000/", timeout=12000)
-    url = httpx.URL(path=request.url.path, query=request.url.query.encode('utf-8'))
-    print(url)
+    client = httpx.AsyncClient(base_url="http://127.0.0.1:8000/", timeout=DEFAULT_TIMEOUT)
+    # get model from request payload
+    model_name = request.json().get('model')
+    print('requested model:', model_name)
+    path = request.url.path.replace('/proxy/', '/')
+    url = httpx.URL(path=path, query=request.url.query.encode('utf-8'))
     req = client.build_request(
         request.method, url, headers=request.headers.raw, content=request.stream()
     )
@@ -21,3 +27,7 @@ async def reverse_proxy(request: Request):
         headers=r.headers,
         background=BackgroundTask(r.aclose)
     )
+
+@app.post('/register')
+async def register_upstream(request: UpstreamRegistrationRequest):
+    uss.append(UpstreamServer(url=request.ip_address, weight=request.weight))
