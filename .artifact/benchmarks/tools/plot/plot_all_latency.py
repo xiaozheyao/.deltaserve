20f261feecdf4d7517442e987735f16e9954252c
Xiaozhe Yao <askxzyao@gmail.com>
2024-05-07 00:30:47 +0200

    cleanup

--- .artifact/benchmarks/tools/plot/plot_all_latency.py
+++ .artifact/benchmarks/tools/plot/plot_all_latency.py
@@ -1,9 +1,20 @@
 import os
 from tqdm import tqdm
 
-files = [x for x in os.listdir(".artifact/benchmarks/results/") if x.endswith(".jsonl")]
+def plot_all(args):
 
-for filename in tqdm(files):
-    os.system(
-        f"python .artifact/benchmarks/tools/plot/plot_latency_per_request.py --output .artifact/benchmarks/results/figures --input .artifact/benchmarks/results/{filename}"
-    )
+    base_url = args.dir
+
+    files = [x for x in os.listdir(args.dir) if x.endswith(".jsonl")]
+
+    for filename in tqdm(files):
+        os.system(
+            f"python .artifact/benchmarks/tools/plot/plot_latency_per_request.py --output .artifact/benchmarks/results/figures/ --input {base_url}/{filename}"
+        )
+
+if __name__ =="__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", type=str, required=True)
+    args = parser.parse_args()
+    plot_all(args)
