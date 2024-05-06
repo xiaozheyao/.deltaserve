1d3ddd4bc12fe71bcef24d87e106df0a5b2c1271
Xiaozhe Yao <askxzyao@gmail.com>
2024-05-06 20:41:04 +0200

    cleanup

--- benchmarks/deltas/test.sh
+++ benchmarks/deltas/test.sh
@@ -1,20 +1,2 @@
-# echo "--------- TP=1 ---------"
-# TP_SIZE=1 python benchmarks/deltas/benchmark_7b.py
-# echo "--------- Not-split weights TP=2 ---------"
-# TP_SIZE=2 UNOPTIMIZED_DELTA=1 python benchmarks/deltas/benchmark_7b.py
-# echo "--------- Split weights ---------"
-# TP_SIZE=2 python benchmarks/deltas/benchmark_7b.py
-
-# echo "--------- TP=1 (bitblas) ---------"
-# TP_SIZE=1 USE_BITBLAS=1 python benchmarks/deltas/benchmark_7b.py
-# echo "--------- TP=2 (bitblas) ---------"
-# TP_SIZE=2 USE_BITBLAS=1 python benchmarks/deltas/benchmark_7b.py
-
-# echo "--------- Split weights ---------"
-# TP_SIZE=2 USE_TRITEIA=1 python benchmarks/deltas/benchmark_7b.py
-
-# echo "--------- TP=1 (triton) ---------"
-# TP_SIZE=1 python benchmarks/deltas/benchmark_7b.py
-
 echo "--------- TP=2 (bitblas, 4bits) ---------"
-BITWIDTH=2 TP_SIZE=2 USE_BITBLAS=1 python benchmarks/deltas/benchmark_7b.py
+BITWIDTH=4 TP_SIZE=2 USE_BITBLAS=1 python benchmarks/deltas/benchmark_7b.py
