391fc5f95cc5db9c995f3f03c09f000d4e9f050d
Xiaozhe Yao <askxzyao@gmail.com>
2024-05-10 22:29:43 +0200

    minor

--- .artifact/scripts/pjlab/submit_chain.py
+++ .artifact/scripts/pjlab/submit_chain.py
@@ -1,8 +1,15 @@
 import os
-
+import subprocess
 SCRIPT_PATH = ".artifact/scripts/pjlab/chains_uniform"
 
 scripts = [x for x in os.listdir(SCRIPT_PATH) if x.endswith(".slurm")]
 
-for script in scripts:
-    job = f""
+for idx, script in enumerate(scripts):
+    previous_id = 0
+    if idx == 0:
+        job = f"sbatch {os.path.join(SCRIPT_PATH, script)}"
+    else:
+        job = f"sbatch --dependency=after:{previous_id}:+5 {os.path.join(SCRIPT_PATH, script)}"
+    previous_id = subprocess.check_output(job, shell=True)
+    previous_id = previous_id.replace("Submitted batch job", "")
+    
