cd4326e6ea970607d3e56dfc82b9ce18f5acd932
Xiaozhe Yao <askxzyao@gmail.com>
2024-05-10 10:12:37 +0200

    add results from pjlab

--- .artifact/benchmarks/tools/plot/plot_ablation.py
+++ .artifact/benchmarks/tools/plot/plot_ablation.py
@@ -14,16 +14,19 @@ def plot(args):
     print(args)
     files = [x for x in os.listdir(args.input) if x.endswith(".jsonl")]
     df = pd.DataFrame()
+    distribution = []
     for file in files:
         metadata, data = parse_data(os.path.join(args.input, file))
+        distribution.append(metadata["distribution"])
         focus_group = [x for x in data if x["type"] in ["E2E Latency", "TTFT"]]
         filename = args.input.split("/")[-1].removesuffix(".jsonl")
         partial_df = pd.DataFrame(focus_group)
-        partial_df["system"], partial_df["order"] = get_short_system_name(metadata)
-        
+        partial_df["system"], partial_df["order"] = get_short_system_name(metadata)        
         df = pd.concat([df, partial_df])
+    distribution = list(set(distribution))
+    assert len(distribution) == 1, "Multiple distributions found"
+    distribution = distribution[0]
     avg_latency = df.groupby(["system", "type"]).mean().reset_index()
-    print(avg_latency)
     # normalized latency to the baseline-1 system
     baseline_latency = avg_latency[avg_latency.system == "Baseline-1"]
     # divide the latency of each type by the baseline-1 latency
@@ -31,8 +34,23 @@ def plot(args):
     avg_latency["speedup"] =  avg_latency["time_baseline"] / avg_latency["time"]
     # order by order
     avg_latency = avg_latency.sort_values(by="order")
-    print(avg_latency)
+    for metric in ['E2E Latency', 'TTFT']:
+        subdf = avg_latency[avg_latency.type == metric]
+        # set line width
+        fig = px.line(subdf, x="system", y="speedup", title='')
+        fig.update_traces(line={'width': 6})
+        set_plotly_theme(fig)
+        fig.update_layout(margin=dict(t=50, l=20, r=10, b=15))
+        set_font(fig)
+        fig.update_yaxes(title_text="Speedup",tickfont=dict(size=36), title_font=dict(size=36))
+        # set font size
+        fig.update_xaxes(title_text="", tickfont=dict(size=36), title_font=dict(size=36))
 
+        fig.write_image(
+            os.path.join(args.output, f"ablation_{distribution}_{metric}.pdf"),
+            width=800,
+            height=500,
+        )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plot latency per request")
