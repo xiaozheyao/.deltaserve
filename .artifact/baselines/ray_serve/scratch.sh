ed0e39688b6fcd3f597f2f4e3668e9e8348735b5
Xiaozhe Yao <askxzyao@gmail.com>
2024-05-10 17:49:39 +0200

    add trace

--- .artifact/baselines/ray_serve/scratch.sh
+++ .artifact/baselines/ray_serve/scratch.sh
@@ -1,5 +1,6 @@
 singularity shell --nv \
     --bind $PWD:/workdir \
     --bind /mnt/scratch/xiayao/cache/HF:/hf \
-    --env TRANSFORMERS_CACHE=/hf \
+    --env HF_HOME=/hf \
+    --workdir /workdir \
     /mnt/scratch/xiayao/cache/images/rayllm.sif
--- .artifact/scripts/sgs/nfs/13b/run_vllm_server_with_4bit_bitblas.sh
+++ .artifact/scripts/sgs/nfs/13b/run_vllm_server_with_4bit_bitblas.sh
@@ -1,4 +1,4 @@
 USE_BITBLAS=1 BITWIDTH=4 python -m vllm.entrypoints.openai.api_server --model meta-llama/Llama-2-13b-hf --enable-delta --disable-log-requests --gpu-memory-utilization 0.85 \
 --delta-modules delta-1=/mnt/scratch/xiayao/projects/fmsys/vllm/.idea/models/13b/4bit/13b.4b75s.tp_2.1 delta-2=/mnt/scratch/xiayao/projects/fmsys/vllm/.idea/models/13b/4bit/13b.4b75s.tp_2.2 delta-3=/mnt/scratch/xiayao/projects/fmsys/vllm/.idea/models/13b/4bit/13b.4b75s.tp_2.3 delta-4=/mnt/scratch/xiayao/projects/fmsys/vllm/.idea/models/13b/4bit/13b.4b75s.tp_2.4 delta-5=/mnt/scratch/xiayao/projects/fmsys/vllm/.idea/models/13b/4bit/13b.4b75s.tp_2.5 delta-6=/mnt/scratch/xiayao/projects/fmsys/vllm/.idea/models/13b/4bit/13b.4b75s.tp_2.6 delta-7=/mnt/scratch/xiayao/projects/fmsys/vllm/.idea/models/13b/4bit/13b.4b75s.tp_2.7 delta-8=/mnt/scratch/xiayao/projects/fmsys/vllm/.idea/models/13b/4bit/13b.4b75s.tp_2.8 \
---max-deltas 4 --max-cpu-deltas 32 --tensor-parallel-size 2 \
+--max-deltas 2 --max-cpu-deltas 32 --tensor-parallel-size 2 \
 --enforce-eager
