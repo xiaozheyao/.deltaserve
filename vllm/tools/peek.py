3ce3e4d08685b35024dce2c78a7b2890e0e6018c
Xiaozhe Yao <askxzyao@gmail.com>
2024-05-15 15:43:47 +0200

    minor

--- vllm/tools/peek.py
+++ vllm/tools/peek.py
@@ -7,7 +7,14 @@ def peek_perf(args):
     short_sys_name = get_short_system_name(metadata)
     print(f"--- {short_sys_name} ---")
     results = pd.DataFrame(results)
-    
+    types = results["type"].unique()
+    for type in types:
+        # average over all runs
+        type_results = results[results["type"]==type]
+        type_results = type_results['time'].mean()
+        print(f"{type}    \t{type_results:.2f} sec")
+        
+        
             
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
