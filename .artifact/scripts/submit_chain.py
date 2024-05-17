ae3ef336d6519649232e955f31c298f34e0f6008
Xiaozhe Yao <askxzyao@gmail.com>
2024-05-17 10:18:04 +0200

    update

--- .artifact/scripts/submit_chain.py
+++ .artifact/scripts/submit_chain.py
@@ -8,10 +8,10 @@ previous_id = 0
 
 for idx, script in enumerate(scripts):
     print(f"Previous ID: {previous_id}")
-    if idx == 0 and previous_id == 0:
+    if previous_id == 0:
         job = f"sbatch {os.path.join(SCRIPT_PATH, script)}"
     else:
         job = f"sbatch --dependency=afterany:{previous_id} {os.path.join(SCRIPT_PATH, script)}"
-    previous_id = subprocess.check_output(job, shell=True, encoding="UTF-8")
-    previous_id = previous_id.replace("Submitted batch job ", "")
-    previous_id = previous_id.replace("\n", "")
+    # previous_id = subprocess.check_output(job, shell=True, encoding="UTF-8")
+    # previous_id = previous_id.replace("Submitted batch job ", "")
+    # previous_id = previous_id.replace("\n", "")
