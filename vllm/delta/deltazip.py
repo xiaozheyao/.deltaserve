bb88fe0515fed72f041ee00785bf73371735309e
Xiaozhe Yao <askxzyao@gmail.com>
2024-05-17 19:35:38 +0200

    minor

--- vllm/delta/deltazip.py
+++ vllm/delta/deltazip.py
@@ -9,7 +9,7 @@ USE_BITBLAS = os.environ.get("USE_BITBLAS", "0") == "1"
 if USE_BITBLAS:
     print("Using bitblas")
     from triteia.ao.ops.linalg.select_matmul.select_bmm import (
-        ibmm as quant_select_bmm_248,
+        vectorized_ibmm as quant_select_bmm_248,
     )
 else:
     from triteia.ao.ops.linalg.select_matmul.select_bmm import quant_select_bmm_248
