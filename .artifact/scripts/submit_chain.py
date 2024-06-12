51015633d3f0af6beb71b8b01b95af176bae5f23
Tonyhao96 <673665244@qq.com>
2024-06-12 21:43:16 +0800

    update

--- .artifact/scripts/submit_chain.py
+++ .artifact/scripts/submit_chain.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-SCRIPT_PATH = ".artifact/scripts/workdir"
+SCRIPT_PATH = ".artifact/scripts/pjlab/e2e"
 
 scripts = [x for x in os.listdir(SCRIPT_PATH) if x.endswith(".slurm")]
 previous_id = 0
@@ -14,5 +14,5 @@ for idx, script in enumerate(scripts):
         job = f"sbatch --dependency=afterany:{previous_id} {os.path.join(SCRIPT_PATH, script)}"
     job_id = subprocess.check_output(job, shell=True, encoding="UTF-8")
     print(job_id)
-    # previous_id = previous_id.replace("Submitted batch job ", "")
-    # previous_id = previous_id.replace("\n", "")
+    previous_id = job_id.replace("Submitted batch job ", "")
+    previous_id = int(previous_id.replace("\n", ""))
