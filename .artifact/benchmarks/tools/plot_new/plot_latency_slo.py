e40c9d544170c6f833918a417674130e407e9f43
Xiaozhe Yao <askxzyao@gmail.com>
2024-06-14 15:34:20 +0200

    timeline figure

--- .artifact/benchmarks/tools/plot_new/plot_latency_slo.py
+++ .artifact/benchmarks/tools/plot_new/plot_latency_slo.py
@@ -6,13 +6,100 @@ import seaborn as sns
 import numpy as np
 import matplotlib
 
+set_matplotlib_style()
+cmp = sns.color_palette("tab10")
+sns.set_style("ticks")
+
+def create_slo_data(df, max_value):
+    assert len(df["sys_name"].unique()) == 1, f"Multiple systems in the dataframe, got {df['sys_name'].unique()}"
+    assert len(df["max_deltas"].unique()) == 1, f"Multiple deltas in the dataframe, got {df['max_deltas'].unique()}"
+    assert len(df["distribution"].unique()) == 1, "Multiple distributions in the dataframe"
+    assert len(df["ar"].unique()) == 1, "Multiple arrival rates in the dataframe"
+    
+    system = df["sys_name"].unique()[0]
+    max_deltas = df['max_deltas'].unique()[0]
+    distribution = df['distribution'].unique()[0]
+    ar = df['ar'].unique()[0]
+    slo_requirements = np.arange(1, max_value, 0.5)
+    slo_data = []
+    if system == 'Baseline-1':
+        sys_name = "Baseline"
+    else:
+        sys_name = f"{system} (N={max_deltas})"
+    for slo in slo_requirements:
+        slo_data.append({
+            "system": sys_name,
+            "slo": slo,
+            "percentage": len(df[df["time"] <= slo]) / len(df),
+            "distribution": distribution,
+            "ar": ar
+        })
+    slo_df = pd.DataFrame(slo_data)
+    return slo_df
+    
 def plot(args):
     SAVEPATH = args.savepath
     set_matplotlib_style()
     full_df = prepare_df(args.path, order=False)
-    metrics = ["E2E Latency","TTFT"]
-    print(full_df)
+    metrics = ["E2E Latency", "TTFT"]
+    for metric in metrics:
+        print(f"Handling {metric}, preparing data...")
+        slo_dfs = []
+        metric_id = metric.lower().replace(" ", "_")
+        met_df = full_df[full_df['type'] == metric]
+        systems = met_df["sys_name"].unique()
+        max_met_value = met_df['time'].max()
+        distributions = met_df["distribution"].unique()
+        for dist in distributions:
+            dist_df = met_df[met_df["distribution"] == dist]
+            arrival_rates = dist_df["ar"].unique()
+            for ar in arrival_rates:
+                ar_df = dist_df[dist_df["ar"] == ar]
+                max_lat = ar_df["time"].max()
+                for system in systems:
+                    sys_df = ar_df[ar_df["sys_name"] == system]
+                    if system == "+Delta":
+                        max_deltas = sys_df["max_deltas"].unique()
+                        for max_delta in max_deltas:
+                            delta_df = sys_df[sys_df["max_deltas"] == max_delta]
+                            slo_dfs.append(create_slo_data(delta_df, max_lat))
+                    else:
+                        slo_dfs.append(create_slo_data(sys_df, max_lat))
 
+        slo_df = pd.concat(slo_dfs)
+        target_distribution = 'azure'
+        target_ar = ['0.5', '2.0']
+        slo_df = slo_df[slo_df['distribution'] == target_distribution]
+        slo_df = slo_df[slo_df['ar'].isin(target_ar)]
+        ar_1_slo = slo_df[slo_df['ar'] == '0.5']
+        ar_2_slo = slo_df[slo_df['ar'] == '2.0']
+        # set index
+        print(ar_1_slo[ar_1_slo['system'] == 'Baseline']['percentage'])
+        
+        grid_params = dict(width_ratios=[1, 1])
+        fig, (ax1, ax2) = plt.subplots(ncols=2, nrows=1, constrained_layout=True, figsize=(9, 3.75))
+        x1, x2 = ar_1_slo['slo'].unique(), ar_2_slo['slo'].unique()
+        
+        linestyles = [":", "-", "--"]
+        ax1.plot(x1, ar_1_slo[ar_1_slo['system'] == 'Baseline']['percentage'], linestyles[0], linewidth=3, alpha=0.9, color=cmp[0], label="Baseline")
+        ax1.plot(x1, ar_1_slo[ar_1_slo['system'] == '+Delta (N=8)']['percentage'], linestyles[1], linewidth=3, alpha=0.9, color=cmp[1], label='+Delta (N=8)')
+        ax1.plot(x1, ar_1_slo[ar_1_slo['system'] == '+Delta (N=12)']['percentage'], linestyles[2], linewidth=3, alpha=0.9, color=cmp[2], label='+Delta (N=12)')
+        
+        ax2.plot(x2, ar_2_slo[ar_2_slo['system'] == 'Baseline']['percentage'], linestyles[0], linewidth=3, alpha=0.9, color=cmp[0], label="Baseline")
+        ax2.plot(x2, ar_2_slo[ar_2_slo['system'] == '+Delta (N=8)']['percentage'], linestyles[1], linewidth=3, alpha=0.9, color=cmp[1], label='+Delta (N=8)')
+        ax2.plot(x2, ar_2_slo[ar_2_slo['system'] == '+Delta (N=12)']['percentage'], linestyles[2], linewidth=3, alpha=0.9, color=cmp[2], label='+Delta (N=12)')
+        
+        ax1.set_xlabel(f"(a) Arrival Rate=0.5")
+        ax2.set_xlabel(f"(b) Arrival Rate=2.0")
+        ax1.set_ylabel(f"Success Rate (%)")
+        ax1.grid(linestyle=":")
+        ax2.grid(linestyle=":")
+        handles, labels = ax1.get_legend_handles_labels()
+        fig.legend(handles=handles, labels=labels, ncols=5, bbox_to_anchor=(0.1, 1.145), loc=2, columnspacing=1.5, handletextpad=0.5)
+        
+        sns.despine()
+        fig.savefig(f"{SAVEPATH}/slo_{metric_id}.pdf", bbox_inches="tight")
+        
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
--- .artifact/benchmarks/tools/plot_new/timeline_sbs.py
+++ .artifact/benchmarks/tools/plot_new/timeline_sbs.py
@@ -12,9 +12,18 @@ cmp = sns.color_palette("tab10")
 
 def get_model_name(model_name):
     if "delta" in model_name:
-        return model_name.replace("delta-", "Model #")
+        return model_name.replace("delta-", "#")
     else:
         return "Base Model"
+
+def get_model_alpha(model_name):
+    if "delta" in model_name:
+        delta_id = int(model_name.split("-")[-1])
+        # delta_id is in [1,16], map to [0.3, 0.9]
+        delta_alpha = 0.3 + 0.045 * delta_id
+        return delta_alpha
+    else:
+        return 1.0
 
 def plot(args):
     dfs = []
@@ -36,11 +45,14 @@ def plot(args):
     df = pd.concat(dfs)
     systems = df['system'].unique()
     fig, (ax1, ax2) = plt.subplots(
-        ncols=2, nrows=1, constrained_layout=True, figsize=(9, 3.75)
+        ncols=2, nrows=1,
+        constrained_layout=True, 
+        figsize=(9, 3.75),
+        sharey=True
     )
     patches = []
     figs = [ax1, ax2]
-    types = ["Queuing", "Inference", "Loading", "TTFT"]
+    types = ["Queuing", "Inference", "Loading"]
     for idx, job_type in enumerate(types):
         patches.append(matplotlib.patches.Patch(color=cmp[idx], label=job_type))
     type_colors = {k: v for k, v in zip(types, cmp)}
@@ -53,14 +65,37 @@ def plot(args):
                 width=row["queueing_end"] - row["queueing_start"],
                 left=row["queueing_start"],
                 color=type_colors["Queuing"],
+                alpha=get_model_alpha(row['model']),
             )
             ax.barh(
                 y=row["id"],
                 width=row["loading_end"] - row["loading_start"],
                 left=row["loading_start"],
                 color=type_colors["Loading"],
+                alpha=get_model_alpha(row['model']),
             )
-    fig.savefig(f"{args.output}/timeline.png", bbox_inches="tight", dpi=300)
+            ax.barh(
+                y=row["id"],
+                width=row["inference_end"] - row["first_token_start"],
+                left=row["first_token_start"],
+                color=type_colors["Inference"],
+                alpha=get_model_alpha(row['model']),
+            )
+            ax.text(
+                x=row["inference_end"] + 0.5,
+                y=row['id'] + 0.25,
+                s=get_model_name(row['model']),
+                fontdict=dict(color='black', fontsize=11),
+            )
+            ax.xaxis.grid(True, alpha=0.5)
+    # add legend to the bottom of the figure, in 3 cols and 1 row
+    ax1.invert_yaxis()
+    plt.legend(
+        handles=patches,
+        fontsize=14,
+    )
+    sns.despine()
+    fig.savefig(f"{args.output}/timeline.pdf", bbox_inches="tight", dpi=300)
 
 if __name__ == "__main__":
     import argparse
